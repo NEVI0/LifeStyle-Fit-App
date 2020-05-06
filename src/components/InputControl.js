0fed73132f17298eb41257f4159f72ecccda643d
Nevio <neviocostamagagnin@gmail.com>
2020-05-06 10:31:46 -0300

    Input Component

---
+++ src/components/InputControl.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
+
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default function InputControl({ label, type, iconName, value, onChangeValue }) {
+    return (
+        <>
+            <Text style={ styles.label }>
+                { label }    
+            </Text>
+
+            <View style={ styles.control }>
+                {
+                    type !== 'password' ?
+                        <TextInput
+                            style={ styles.input }
+                            value={ value }
+                            autoCapitalize="none"
+                            autoCorrect={ false }
+                            onChangeText={ text => onChangeValue(text) }
+                        />
+                    :
+                        <TextInput
+                            style={ styles.input }
+                            value={ value }
+                            secureTextEntry={ true }
+                            autoCorrect={ false }
+                            autoCapitalize="none"
+                            onChangeText={ text => onChangeValue(text) }
+                        />
+                }                
+                <MaterialIcons name={ iconName } size={ 25 } style={ styles.icon } />
+            </View>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    label: {
+        marginBottom: 5,
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    control: {
+        backgroundColor: '#fff',
+        paddingVertical: 5,
+        paddingHorizontal: 7,
+        borderRadius: 4,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+        elevation: 7,
+        marginBottom: 10,
+        flexDirection: 'row'
+    },
+    input: {
+        flex: 1
+    },
+    icon: {
+        alignSelf: 'center',
+        marginLeft: 5
+    },
+});
