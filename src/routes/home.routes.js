70e618d072e1d5f4575a495d0140f12e769a872d
Nevio <neviocostamagagnin@gmail.com>
2020-05-06 10:30:43 -0300

    Stack Home Routes

---
+++ src/routes/home.routes.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import HomePage from '../screens/HomePage';
+import TrackCreate from '../screens/TrackCreate';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+const HomeStack = createStackNavigator();
+
+export default function HomeRoutes({ route, navigation }) {
+
+    if (route.state && route.state.index > 0) {
+        navigation.setOptions({ tabBarVisible: false });
+    } else {
+        navigation.setOptions({ tabBarVisible: true });
+    }
+
+    return (
+        <HomeStack.Navigator>
+            <HomeStack.Screen
+                name="HomePage"
+                component={ HomePage }
+                options={{
+                    headerTintColor: '#1cdbb5',
+                    headerTitleAlign: 'center',
+                    headerTitle: 'Home Page',
+                    headerTitleStyle: {
+                        fontFamily: 'permanent-maker',
+                        fontSize: 30
+                    }                    
+                }}
+            />
+            <HomeStack.Screen
+                name="TrackCreate"
+                component={ TrackCreate }
+                options={{
+                    headerTitleAlign: 'center',
+                    headerTitleStyle: {
+                        fontFamily: 'permanent-maker',
+                        fontSize: 30
+                    }
+                }}
+            />
+        </HomeStack.Navigator>
+    );
+
+}
