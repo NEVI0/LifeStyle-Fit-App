05665f00e62bd50f511650d03fe8e955ef011285
Nevio <neviocostamagagnin@gmail.com>
2020-05-15 10:43:02 -0300

    List All Tracks Screen

--- src/screens/ListAllTracks.js
+++ src/screens/ListAllTracks.js
@@ -1,28 +1,51 @@
-import React from 'react';
-import { StyleSheet, ScrollView, Text, TouchableOpacity, FlatList } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
 
 import { FontAwesome5 } from '@expo/vector-icons';
 
+import TrackContext from '../contexts/track.context';
+import AuthContext from '../contexts/auth.context';
+
+import FilterButton from '../components/FilterButton';
+import ItemList from '../components/ItemList';
+
 export default function ListAllTracks({ navigation }) {
+
+	const { user } = useContext(AuthContext);
+	const { allTracks, trackLoading, getTracks } = useContext(TrackContext);
 
 	navigation.setOptions({
         headerLeft: () => (
-            <TouchableOpacity style={ styles.headerBtn } onPress={ () => navigation.pop() }>
+            <TouchableOpacity style={ styles.headerBtn } onPress={ () => navigation.navigate('HomePage') }>
                 <FontAwesome5 name="angle-double-left" size={ 25 } />
             </TouchableOpacity>
         )
 	});
 
+	if (navigation.isFocused()) {
+		getTracks({ userId: user._id });
+	}
+
 	return (
-		<ScrollView>
-			<Text>SFDGFDGDFGDFGDFGDFG</Text>
-		</ScrollView>
+		<View style={ styles.container }>
+			
+			<FlatList
+				data={ allTracks }
+				keyExtractor={ item => item._id }
+				renderItem={ ({ item }) => <ItemList item={ item } page="list" /> }
+				showsVerticalScrollIndicator={ false }
+			/>
+
+			<FilterButton /> 
+		</View>
 	);
 
 }
 
 const styles = StyleSheet.create({
-	container: {},
+	container: {
+		flex: 1
+	},
 	headerBtn: {
         marginLeft: 15,
         marginTop: 5
