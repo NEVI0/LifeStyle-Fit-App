9dc0fc650df79ca194778544aeb41f150bc6658f
Nevio <neviocostamagagnin@gmail.com>
2020-05-17 13:32:57 -0300

    Adding Scroll View and Search Box

--- src/screens/ListAllTracks.js
+++ src/screens/ListAllTracks.js
@@ -1,18 +1,17 @@
 import React, { useContext } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+import { StyleSheet, View, ScrollView,TextInput, TouchableOpacity, FlatList } from 'react-native';
 
 import { FontAwesome5 } from '@expo/vector-icons';
 
 import TrackContext from '../contexts/track.context';
 import AuthContext from '../contexts/auth.context';
 
-import FilterButton from '../components/FilterButton';
-import ItemList from '../components/ItemList';
+import ViewAllItemList from '../components/ViewAllItemList';
 
 export default function ListAllTracks({ navigation }) {
 
 	const { user } = useContext(AuthContext);
-	const { allTracks, trackLoading, getTracks } = useContext(TrackContext);
+	const { allTracks, getTracks } = useContext(TrackContext);
 
 	navigation.setOptions({
         headerLeft: () => (
@@ -27,17 +26,24 @@ export default function ListAllTracks({ navigation }) {
 	}
 
 	return (
-		<View style={ styles.container }>
+		<ScrollView style={ styles.container }>
 			
+			<View style={ styles.searchBox }>
+				<TextInput
+					style={ styles.input }
+					placeholder="Pesquise um exercício..."
+				/>
+				<FontAwesome5 name="search" size={ 20 } color="#a0dd11" />
+			</View>
+
 			<FlatList
 				data={ allTracks }
 				keyExtractor={ item => item._id }
-				renderItem={ ({ item }) => <ItemList item={ item } page="list" /> }
+				renderItem={ ({ item }) => <ViewAllItemList item={ item } /> }
 				showsVerticalScrollIndicator={ false }
 			/>
 
-			<FilterButton /> 
-		</View>
+		</ScrollView>
 	);
 
 }
@@ -49,5 +55,28 @@ const styles = StyleSheet.create({
 	headerBtn: {
         marginLeft: 15,
         marginTop: 5
-    }
+    },
+	searchBox: {
+		backgroundColor: '#fff',
+        paddingVertical: 6,
+        paddingHorizontal: 8,
+        borderRadius: 4,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+		elevation: 7,
+		marginHorizontal: 20,
+		marginTop: 15,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	input: {
+		flex: 1,
+		color: '#000'
+	}
 });
