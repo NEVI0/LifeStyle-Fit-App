0231ef7396f489a68324978c03118dec65a942b2
Nevio <neviocostamagagnin@gmail.com>
2020-05-22 11:52:42 -0300

    Mocked Data

---
+++ src/mocked/tipsData.js
@@ -0,0 +1,34 @@
+const tipsData = [
+	{
+		id: 1,
+		title: 'Caminhada',
+		tips: {
+			tip1: 'Comece com uma meta pequena de 15 a 20 minutos por dia',
+			tip2: 'Comece sua caminhada para emagrecer devagar, como um aquecimento, aumente a velocidade no meio e diminua no final para esfriar o corpo',
+			tip3: 'Caminhe mais que uma vez por dia',
+		},
+		image: ''
+	},
+	{
+		id: 2,
+		title: 'Corrida',
+		tips: {
+			tip1: 'Aqueça e alongue antes e depois dos treinos',
+			tip2: 'Aumente o ritmo gradativamente durante o treino',
+			tip3: 'Faça uma refeição leve duas horas antes do exercício',
+		},
+		image: ''
+	},
+	{
+		id: 3,
+		title: 'Ciclismo',
+		tips: {
+			tip1: 'Pedale com a postura correta',
+			tip2: 'Use roupas e equipamentos adequados',
+			tip3: 'Vá pedalar com um grupo, é mais seguro e divertido',
+		},
+		image: ''
+	}
+];
+
+export default tipsData;
