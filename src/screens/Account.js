dd24dbe084e92d80c2b7e42c5a7afcabbf2ddd58
Nevio <neviocostamagagnin@gmail.com>
2020-05-25 17:12:30 -0300

    Improving Account Screen

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,7 +1,9 @@
 import React, { useContext, useState } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, ScrollView } from 'react-native';
 
-import { MaterialIcons, FontAwesome } from '@expo/vector-icons';
+import * as app from '../../app.json';
+
+import { MaterialIcons, FontAwesome, AntDesign } from '@expo/vector-icons';
 
 import AuthContext from '../contexts/auth.context';
 
@@ -9,7 +11,7 @@ import CustomModal from '../components/Modal';
 
 export default function Account({ navigation }) {
 
-    const [ isVisible, setIsVisible ] = useState(false);
+	const [ isVisible, setIsVisible ] = useState(false);
 
     const { user, signOut } = useContext(AuthContext);
 
@@ -26,6 +28,8 @@ export default function Account({ navigation }) {
 	
 	return (
         <ScrollView style={ styles.container }>
+
+			{ isVisible ? <CustomModal title="Alerta!" text="Tem certeza que quer sair do app?" onCloseModal={ () => setIsVisible(false) } onDoAction={ () => signOut() } /> : null }
             
             <View style={ styles.userInfo }>
                 <View style={ styles.avatar }>
@@ -36,16 +40,35 @@ export default function Account({ navigation }) {
                 <Text>{ user.email }</Text>
             </View>
 
-			{ 
-				isVisible ? 
-					<CustomModal 
-						title="Alerta!" 
-						text="Tem certeza que quer sair do app?" 
-						onCloseModal={ () => setIsVisible(false) }
-						onDoAction={ () => signOut() } 
-					/> 
-				: null
-			}
+			<View style={ styles.card }>
+				<Text style={[ styles.title, { color: '#2db2c6' } ]}>ESTATÍSTICAS</Text>
+			
+			</View>
+
+			<View style={ styles.card }>
+				<Text style={[ styles.title, { color: '#f9bc13' } ]}>CONFIGURAÇÕES</Text>
+				
+				<View style={ styles.section }>
+					<Text style={ styles.sectionText }>Recuperar Senha...</Text>
+
+					<TouchableOpacity style={ styles.sectionBtn }>
+						<AntDesign name="unlock" size={ 22 } color="#0be02e" />
+					</TouchableOpacity>
+				</View>
+			
+				<View style={ styles.section }>
+					<Text style={ styles.sectionText }>Excluir Conta...</Text>
+
+					<TouchableOpacity style={ styles.sectionBtn }>
+						<AntDesign name="deleteuser" size={ 22 } color="#f74747" />
+					</TouchableOpacity>
+				</View>
+			</View>
+
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-end', padding: 10 }}>
+				<Text style={{ color: '#cecece' }}>Company: Dev Inc.</Text>
+				<Text style={{ color: '#cecece' }}>Versão: { app.expo.version }</Text>
+			</View>
         
         </ScrollView>
     );
@@ -61,7 +84,7 @@ const styles = StyleSheet.create({
     },
     userInfo: {
         padding: 10,
-        marginVertical: 20,
+        marginVertical: 10,
 		alignItems: 'center',
     },
     avatar: {
@@ -79,4 +102,43 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginTop: 15
 	},
+	card: {
+		margin: 15,
+		backgroundColor: '#fff',
+		padding: 10,
+		borderRadius: 10,
+		marginBottom: 20,
+		shadowColor: '#1cdbb5',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+		elevation: 5,
+	},
+	title: {
+		fontSize: 20,
+		textAlign: 'center',
+		marginVertical: 10,
+		fontFamily: 'permanent-maker'
+	},
+	section: {
+		width: '100%',
+		flexDirection: 'row',
+		padding: 10,
+		alignItems: 'center'
+	},
+	sectionText: {
+		fontSize: 14,
+		fontWeight: 'bold',
+		flex: 1
+	},
+	sectionBtn: {
+		paddingVertical: 5,
+		paddingHorizontal: 10,
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: 4,
+	}
 });
